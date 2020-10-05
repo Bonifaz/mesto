1279180a5b554e0a7be12eefad2a10a2200df8a2
Jelizaveta Mahrova <Bonifazyy@yandex.ru>
2020-10-05 17:34:00 +0300

    Исправлено попытка 2

--- script/script.js
+++ script/script.js
@@ -1,43 +1,32 @@
 let buttonProfile = document.querySelector('.profile__edit-button');
-let form = document.querySelector('.form');
-let overlay = document.querySelector('.overlay');
-let formName = document.querySelector('.form__item-name');
+let formContainer = document.querySelector('.form-container');
+let formName = document.querySelector('.form__item_name');
 let name = document.querySelector('.profile__info-title');
 let formSubtitle = document.querySelector('.form__item-subtitle');
 let subtitle = document.querySelector('.profile__info-subtitle');
-
-console.log(formName);
+let buttonSave = document.querySelector('.form__button');
+let buttonCross = document.querySelector('.form__cross');
+let form = document.querySelector('.form');
 
 buttonProfile.addEventListener('click', openFormHandler);
 
 function openFormHandler(){
-    form.classList.remove('form__close');
-    form.classList.remove('overlay__close');
-    form.classList.add('form__open');
-    overlay.classList.add('overlay__open');
-    formName.setAttribute('value', name.textContent);
-    formSubtitle.setAttribute('value', subtitle.textContent);
+    formContainer.classList.add("form-container_open");
+    formName.value =  name.textContent;
+    formSubtitle.value = subtitle.textContent;
 }
-
-let buttonCross = document.querySelector('.form__cross');
 
 buttonCross.addEventListener('click', closeFormHandler);
 
 function closeFormHandler(){
-    form.classList.remove('form__open');
-    form.classList.remove('overlay__open');
-    form.classList.add('form__close');
-    overlay.classList.add('overlay__close');
+    formContainer.classList.remove("form-container_open");
 }
 
-let buttonSave = document.querySelector('.form__button');
+form.addEventListener('submit', saveFormHandler);
 
-buttonSave.addEventListener('click', saveFormHandler);
-
-function saveFormHandler(){
-   name.textContent = formName.value;
-   subtitle.textContent = formSubtitle.value;
-   form.classList.add('form__close');
-    overlay.classList.add('overlay__close');
+function saveFormHandler(evt){
+    evt.preventDefault();
+    name.textContent = formName.value;
+    subtitle.textContent = formSubtitle.value;
+    closeFormHandler();
 }
-
