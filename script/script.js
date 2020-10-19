369c9ec2a609941cf47a7b5dde773bc4ee72514a
Jelizaveta Mahrova <Bonifazyy@yandex.ru>
2020-10-19 14:59:44 +0300

    Проектная работа 5

--- script/script.js
+++ script/script.js
@@ -1,21 +1,72 @@
 let buttonProfile = document.querySelector('.profile__edit-button');
-let formContainer = document.querySelector('.pop-up');
+let formContainerEdit = document.querySelector('.pop-up_edit');
 let formName = document.querySelector('.pop-up__item_name');
 let name = document.querySelector('.profile__info-title');
 let formSubtitle = document.querySelector('.pop-up__item_subtitle');
 let subtitle = document.querySelector('.profile__info-subtitle');
-let buttonSave = document.querySelector('.pop-up__button');
-let buttonCross = document.querySelector('.pop-up__cross');
-let form = document.querySelector('.pop-up__form');
+let buttonSave = document.querySelector('.pop-up__button_edit');
+let buttonCross = document.querySelector('.pop-up__cross_edit');
+let form = document.querySelector('.pop-up__form_edit');
+
+const cardTemplate = document.querySelector('#template').content;
+const cardImage = document.querySelector('.element__image');
+
+const elements = document.querySelector('.elements');
+const cardTitle = document.querySelector('.element__title');
+const cardInfo = document.querySelector('.element__card-info');
+
+const formContainerAdd = document.querySelector('.pop-up_add');
+const formPlace = document.querySelector('.pop-up__item-place');
+const formLink = document.querySelector('.pop-up__item-link');
+const buttonAdd = document.querySelector('.profile__button');
+const buttonCrossAdd = document.querySelector('.pop-up__cross_add');
+const buttonSaveAdd = document.querySelector('.pop-up__button_add');
+const formAdd = document.querySelector('.pop-up__form_add');
+
+const modalWindowImage = document.querySelector('.pop-up__modal-image');
+const closeImageButton = document.querySelector('.pop-up__close-icon');
+const modalImage = document.querySelector('.pop-up__image');
+const modalImageTitle = document.querySelector('.pop-up__bottom-title');
+
+
+ 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+    
+]; 
+
 
 function openFormHandler(){
-    formContainer.classList.add("pop-up_open");
+    formContainerEdit.classList.add("pop-up_open");
     formName.value =  name.textContent;
     formSubtitle.value = subtitle.textContent;
 }
 
 function closeFormHandler(){
-    formContainer.classList.remove("pop-up_open");
+    formContainerEdit.classList.remove("pop-up_open");
 }
 
 function saveFormHandler(evt){
@@ -25,6 +76,74 @@ function saveFormHandler(evt){
     closeFormHandler();
 }
 
+
+initialCards.forEach(function (item){
+    const templateCopy = cardTemplate.cloneNode(true);
+    const buttonHeart = templateCopy.querySelector('.element__heart');
+    const buttonDeleteCard = templateCopy.querySelector('.element__delete');
+    templateCopy.querySelector('.element__image').src = item.link;
+    templateCopy.querySelector('.element__title').textContent = item.name;
+    
+    buttonDeleteCard.addEventListener('click',() => deleteCardHandler(buttonDeleteCard));
+    buttonHeart.addEventListener('click',() => buttonHeartActiveHandler(buttonHeart));
+
+    const openImageButton = templateCopy.querySelector('.element__image');
+    openImageButton.addEventListener('click',() => openImageHandler(item.link, item.name));
+
+    elements.prepend(templateCopy);
+});
+
+function deleteCardHandler(buttonDeleteCard){
+    const deleteCardClass = buttonDeleteCard.closest('.element');
+    deleteCardClass.remove();
+}
+
+function buttonHeartActiveHandler(buttonHeart){
+    buttonHeart.classList.toggle("element__heart_active");
+}
+
+function openFormAddHandler(){
+    formContainerAdd.classList.add("pop-up_open");
+}
+
+function closeFormAddHandler(){
+    formContainerAdd.classList.remove("pop-up_open");
+}
+
+function saveFormAddHandler(evt){
+    evt.preventDefault();
+    const templateCopy = cardTemplate.cloneNode(true);
+    templateCopy.querySelector('.element__image').src = document.querySelector('.pop-up__item-link').value;
+    templateCopy.querySelector('.element__title').textContent = document.querySelector('.pop-up__item-place').value;
+    const buttonHeart = templateCopy.querySelector('.element__heart');
+    const buttonDeleteCard = templateCopy.querySelector('.element__delete');
+    buttonDeleteCard.addEventListener('click',() => deleteCardHandler(buttonDeleteCard));
+    buttonHeart.addEventListener('click',() => buttonHeartActiveHandler(buttonHeart));
+
+    const openImageButton = templateCopy.querySelector('.element__image');
+    openImageButton.addEventListener('click',() => openImageHandler(formLink.value, formPlace.value));
+    
+    elements.prepend(templateCopy);
+    closeFormAddHandler();
+}
+
+function openImageHandler(link, name){
+    modalImage.src = link;
+    modalImageTitle.textContent = name;
+    modalWindowImage.classList.toggle("pop-up__modal-image_open");
+}
+
+function closeImageHandler(){
+    modalWindowImage.classList.toggle("pop-up__modal-image_open");
+}
+
+
 buttonProfile.addEventListener('click', openFormHandler);
 form.addEventListener('submit', saveFormHandler);
 buttonCross.addEventListener('click', closeFormHandler);
+
+buttonAdd.addEventListener('click', openFormAddHandler);
+buttonCrossAdd.addEventListener('click', closeFormAddHandler);
+formAdd.addEventListener('submit', saveFormAddHandler);
+
+closeImageButton.addEventListener('click',() => closeImageHandler() );
